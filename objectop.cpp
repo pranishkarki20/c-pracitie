e98609e9f81182f63b735ac9b0183d6e08e31884
Pranish Karki <praniskkarki20@gmail.com>
2025-09-30 22:30:42 +0530

    qweqw

--- objectop.cpp
+++ objectop.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std; 
 
+class employee 
+{
+    private :
+    int  salarly ;
+    public :
+    string  name ;
+    char position ;
+    void setdata(int salarl ) {
+        salarly = salarl ;
+    };
+    void getdata(){
+        cout << "the value of a is " << name << endl;
+         cout << "the salarly of ram ud " << salarly;
+    };
+
+} harry ;
+
+int main(){ 
+    harry.name =("ram");
+    harry.setdata(100000);
+    harry.getdata();
+}
