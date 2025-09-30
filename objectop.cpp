c2e6f4d59fa479eca65d3aed3ef162625604ae3f
Pranish Karki <praniskkarki20@gmail.com>
2025-09-30 22:14:26 +0530

    fff

--- objectop.cpp
+++ objectop.cpp
@@ -1,25 +1,3 @@
 #include <iostream>
 using namespace std; 
 
-class employee 
-{
-    private :
-    int  salarly ;
-    public :
-    string  name ;
-    char position ;
-    void setdata(int salarl );
-    void getdata(){
-        cout << "the value of a is " << name << endl;
-    };
-
-} harry ;
-void employee :: setdata( int salarl ){
-     salarly = salarly;
-    
-}
-int main(){ 
-    harry.name =("ram");
-    harry.setdata(100000);
-    harry.getdata();
-}
