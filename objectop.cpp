2244bd4db4df299cc5e93fc979f3219f88dd7041
Pranish Karki <praniskkarki20@gmail.com>
2025-09-30 20:36:39 +0530

    Add files via upload

---
+++ objectop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std; 
+
+class employee 
+{
+    private :
+    int  salarly ;
+    public :
+    string  name ;
+    char position ;
+    void setdata(int salarl );
+    void getdata(){
+        cout << "the value of a is " << name << endl;
+    };
+
+} harry ;
+void employee :: setdata( int salarl ){
+     salarly = salarly;
+    
+}
+int main(){ 
+    harry.name =("aam");
+    harry.setdata(100000);
+    harry.getdata();
+}
